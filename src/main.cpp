52dee8acf847b871113df7d66d06a7ac1c96ca97
Phil[go] <philipe.ng@protonmail.com>
2020-06-11 20:41:08 -0400

    Day 9 Challenge

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,32 @@
-#pragma region Day 9 Challenge - MATCHES
-/******* DAY 09 - MATCHES - Playing with Matches  ********
-Chef's son Chefu found some matches in the kitchen and he immediately 
-starting playing with them.
-The first thing Chefu wanted to do was to calculate the result of his 
-homework — the sum of A and B, and write it using matches. Help Chefu 
-and tell him the number of matches needed to write the result.
-Constraints: 
-    1 <= T <= 1000
-    1 <= A, B <= 1000000
-    Time limit: 1sec
-    Source Limit: 50KB
-*/
+#pragma region Day 10 Challenge - 
 
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-int Checker(int a, int b)
+int Checker(long a)
 {
-    std::string s = std::to_string(a + b);
+    std::string s = std::to_string(a);
     int res = 0;
     for (int it = 0; it < s.length(); it++)
     {
-        switch (s[it])
-        {
-        case '8':
-            res += 7;
-            break;
-        case '1':
-            res += 2;
-            break;
-        case '4':
-            res += 4;
-            break;
-        case '7':
-            res += 3;
-            break;
-        default:
-        {
-            if (s[it] == '2' || s[it] == '3' || s[it] == '5')
-                res += 5;
-            else
-                res += 6;
-        }
-        break;
-        }
+        s[it] == '4' ? res ++ : res;
     }
     return res;
 }
 
 int main(int argc, char *agrv[])
 {
-    short t;
-    int a, b;
+    int t;
+    long n;
     std::vector<int> result;
 
     scanf("%d", &t);
     for (t; t > 0; t--)
     {
-        scanf("%d %d", &a, &b);
-        result.push_back(Checker(a, b));
+        scanf("%d", &n);
+        result.push_back(Checker(n));
     }
 
     for (int i = 0; i < result.size(); i++)
