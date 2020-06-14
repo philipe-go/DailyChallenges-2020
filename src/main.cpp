f848970c76e45c842a007059cbfe06648dbea011
Phil[go] <philipe.ng@protonmail.com>
2020-06-13 22:47:10 -0400

    Day 13 challenge

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,15 @@
-#include <string>
-#include <vector>
+#include <stdio.h>
 #include <iostream>
-
-using namespace std;
-
-string WordChecker(string s1, string s2)
-{
-    int min = s1.length(), max = 0;
-    for (int i = 0; i < s1.length(); i++)
-    {
-        switch (s1.at(i))
-        {
-            case '?':
-            {
-                min--;
-                max++;
-            }break;
-            default:
-            {
-                if (s2.at(i) == '?') { min--; max++;}
-                else if (s1.at(i) == s2.at(i)) min--;
-                else max++;
-            }
-        }
-
-    }
-    return (to_string(min) + " " + to_string(max));
-}
 
 int main(int argc, char *agrv[])
 {
-    short t;
-    string s1, s2;
-    vector<string> result;
+    short t, a, b;
 
-    cin >> t;
+    scanf("%d%", &t);
     for (t; t > 0; t--)
     {
-        cin >> s1;
-        cin >> s2;
-        result.push_back(WordChecker(s1,s2));
-    }
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        cout << result[i] << endl;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a%b);
     }
 
     system("pause");
