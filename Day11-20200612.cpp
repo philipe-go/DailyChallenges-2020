df025a77deaac8dba7bb7d15ac3f4b2ae5501942
Phil[go] <philipe.ng@protonmail.com>
2020-06-15 10:26:56 -0400

    Day 11 challenge corrected

--- Day11-20200612.cpp
+++ Day11-20200612.cpp
@@ -1,4 +1,4 @@
-#pragma region Day 11 Challenge -  TRISQ
+#pragma region Day 11 Challenge - TRISQ
 /******* DAY 11 - TRISQ - Fit Squares in Triangle   ********
 What is the maximum number of squares of size 2x2 that can be fit
  in a right angled isosceles triangle of base B.
@@ -13,35 +13,26 @@ Constraints:
 
 #include <stdio.h>
 #include <iostream>
-#include <string>
-#include <vector>
 
 int Triangle(int b)
 {
-   if ( b / 2 > 2 ) return (b - 1) * (b - 1) + Triangle (b / 2);
-   return 0;  
+    b % 2 != 0 ? b-- : b;
+    return (b * b - 2 * b) / 8;
 }
 
 int main(int argc, char *agrv[])
 {
     int t, b;
-    std::vector<int> result;
 
     scanf("%d", &t);
     for (t; t > 0; t--)
     {
         scanf("%d", &b);
-        result.push_back(Triangle(b));
-    }
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        printf("%d\n", result[i]);
+        printf("%d\n", Triangle(b));
     }
 
     system("pause");
     return 0;
 }
-
 
 #pragma endregion
