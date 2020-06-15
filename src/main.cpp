3e401ad1be4203d21ea07c8c006f79730ff3e67c
Phil[go] <philipe.ng@protonmail.com>
2020-06-15 11:48:32 -0400

    Day 14 challenge

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,56 @@
-#pragma region Day 11 Challenge - TRISQ
-/******* DAY 11 - TRISQ - Fit Squares in Triangle   ********
-What is the maximum number of squares of size 2x2 that can be fit
- in a right angled isosceles triangle of base B.
-One side of the square must be parallel to the base of the isosceles triangle.
-Base is the shortest side of the triangle
+#pragma region Day 14 Challenge - PRICECON
+/******* DAY 14 - PRICECON - Chef and Price Control   ********
+Chef has N items in his shop (numbered 1 through N); for each valid i, 
+the price of the i-th item is Pi. Since Chef has very loyal customers, all N
+items are guaranteed to be sold regardless of their price.
+However, the government introduced a price ceiling K, which means that for 
+each item i such that Pi>K, its price should be reduced from Pi to K.
+Chef's revenue is the sum of prices of all the items he sells. Find the amount 
+of revenue which Chef loses because of this price ceiling.
+
 Constraints: 
-    1 <= T <= 10^3
-    1 <= B <= 10^4
+    1 <= T <= 100
+    1 <= N <= 10000
+    1 <= Pi <= 1000 for each value of i
+    1 <= K <= 1000
+    Subtask #1 (100 points): original constraints
     Time limit: 1sec
     Source Limit: 50KB
 */
 
 #include <stdio.h>
+#include <vector>
 #include <iostream>
 
-int Triangle(int b)
+int RevenueChecker(std::vector<short> rev, short k)
 {
-    b % 2 != 0 ? b-- : b;
-    return (b * b - 2 * b) / 8;
+    int lostRev = 0;
+    while(!rev.empty())
+    {
+        rev.back() < k ? lostRev : lostRev += rev.back() - k;
+        rev.pop_back();
+    }
+    return lostRev;
 }
 
-int main(int argc, char *agrv[])
+int main(int argc, char *argv[])
 {
-    int t, b;
+    short t;
+    int n, p, k;
+    std::vector<short> rev;
 
     scanf("%d", &t);
-    for (t; t > 0; t--)
+    while (t--)
     {
-        scanf("%d", &b);
-        printf("%d\n", Triangle(b));
+        scanf("%d", &n);
+        scanf("%d", &k);
+        while(n--)
+        {
+            scanf("%d", &p);
+            rev.push_back(p);
+        }
+            printf("%d\n", RevenueChecker(rev, k));
+            rev.clear();
     }
 
     system("pause");
